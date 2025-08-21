02eca573e1a0888bbe3d65350c3df49b5156d394
Ahsan1447 <ahsanchaudary1447@gmail.com>
2025-08-21 19:30:31 +0500

    Fixed background color of widget page

--- src/app/search/page.jsx
+++ src/app/search/page.jsx
@@ -6,7 +6,7 @@ import SignOutButton from '@/app/components/SignOutButton'
 export default function SearchPage() {
   return (
     <div>
-      <div className="w-full flex justify-end px-8 py-4 bg-custom-blue-50">
+      <div className="w-full flex justify-end px-8 py-4 bg-gray-100">
         <SignOutButton />
       </div>
       <SearchWidget />
